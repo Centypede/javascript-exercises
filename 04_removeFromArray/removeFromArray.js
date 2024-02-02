0666aa8e1d639108c0ac001440334a8c6e845e8e
Centi <154397079+Centypede@users.noreply.github.com>
2024-02-01 21:50:16 -0700

    Finish exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,6 @@
-const removeFromArray = function() {
-
-};
-
+const removeFromArray = function(arr1, ...removed) {
+    const arr2 = arr1.filter(number => !removed.includes(number));
+    return arr2;
+}
 // Do not edit below this line
 module.exports = removeFromArray;
