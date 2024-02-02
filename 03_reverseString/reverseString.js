3edfed8f616837e6ab3dc9fda5ebf965a3282465
Centi <154397079+Centypede@users.noreply.github.com>
2024-02-01 19:35:09 -0700

    Finish exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,21 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    // My solution
+    if (typeof str !== "string") {
+        return "ERROR";
+    } 
+    newStr = "";
+    i = 0;
+    for (i = 0 + (str.length - 1); i >= 0; i--) {
+        newStr += str[i];
+        console.log(newStr);
+    }
+    return newStr;
 };
+
+// simpler given solution
+
+//     return str.split("").reverse().join("");
+// }
 
 // Do not edit below this line
 module.exports = reverseString;
