994a9a3c4c14f65f1ce4872cb5f842c985888f7f
Centi <154397079+Centypede@users.noreply.github.com>
2024-02-01 19:30:18 -0700

    Finish exercises 1+2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
+    let i = 0;
+    let str = '';
+    while (i < num) {
+        i++;
+        str += string;
+    } 
+    return str;
 };
+
+repeatString();
 
 // Do not edit below this line
 module.exports = repeatString;
