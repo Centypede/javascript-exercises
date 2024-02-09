39e4bd68d3722f8ec96b49c09b4c834379828ccf
Centi <154397079+Centypede@users.noreply.github.com>
2024-02-08 20:06:33 -0700

    Finish exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (typeof num1 != "number" || typeof num2 != "number" || num1 < 0 || num2 < 0) {
+        return "ERROR";
+    }
 
+    let start = num1;
+    let end = num2;
+
+    if (num2 < num1) {
+        start = num2;
+        end = num1;
+    }
+
+    let i = start;
+    let sum = 0;
+
+    while (i <= end) {
+        sum += i;
+        i++
+    }
+    
+    return sum;
 };
 
 // Do not edit below this line
